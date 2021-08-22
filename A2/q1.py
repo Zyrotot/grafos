90f24d985aa6787659d36fe293bef4d3925d7c36
Zyrotot <mathiasdutranichele@gmail.com>
2021-08-22 11:46:02 -0300

    Terminando Q1 e Q2

--- A2/q1.py
+++ A2/q1.py
@@ -19,8 +19,29 @@ def buscaProfundidade(grafo):
         tempoInicio[vertice] = math.inf
         tempoTermino[vertice] = math.inf
         antecessor[vertice] = None
+        time = 0
 
     for vertice in range(1,  grafo.tamanho+1):
+        if visitado[vertice] == False:
+            visitaProfundidade(grafo, vertice)
+ 
+    return (visitado, tempoInicio, antecessor, tempoTermino)
+
+def buscaProfundidadeAdaptado(grafo, sortTempo):
+    global visitado
+    global tempoInicio
+    global tempoTermino
+    global antecessor
+    global time
+
+    for vertice in range(1,  grafo.tamanho+1):
+        visitado[vertice] = False
+        tempoInicio[vertice] = math.inf
+        tempoTermino[vertice] = math.inf
+        antecessor[vertice] = None
+        time = 0
+
+    for vertice in sortTempo:
         if visitado[vertice] == False:
             visitaProfundidade(grafo, vertice)
  
@@ -50,35 +71,67 @@ def visitaProfundidade(grafo, vertice):
     return True
 
 def transporAresta(grafo):
-    arestasT = []
-    for aresta in grafo.arestas:
-        arestaT = [aresta[1], aresta[0]]
-        arestasT.append(arestaT)
+    arestasT = {}
+    arestas = grafo.arestas
+    for aresta in arestas:
+        arestasT[aresta[1], aresta[0]] = arestas[aresta]
         
     grafo.arestas = arestasT
 
     return grafo
-'''
+
+def buscaAntecessor(vertice, antecessores):
+    con = set()
+    for antecessor in antecessores:
+        if vertice == antecessores[antecessor]:
+            con.add(vertice)
+            con.add(antecessor)
+            con = con.union(buscaAntecessor(antecessor, antecessores))
+            
+    return con
+
 G = Grafo(arquivo='teste1.grafo')
 
-buscaProfundidade(G)
+
+result = {}
+result['visitado'], result['tempoInicio'], result['antecessor'], result['tempoTermino'] = buscaProfundidade(G)
+print(result)
+
+print(G.arestas)
 
 print(tempoInicio)
 print(tempoTermino)
 
+sortTempo = {k: v for k, v in sorted(tempoTermino.items(), key=lambda item: item[1], reverse=True)}
 
-#debug
-visitados, distancias, antecessores = G.busca()
+print(sortTempo)
 
-print('Visitados:')
-for vertice, visitado in visitados.items():
-    print(vertice, '->', visitado)
+Gt = transporAresta(G)
 
-print('Distancia:')
-for vertice, distancia in distancias.items():
-    print(vertice, '->', distancia)
+resultT = {}
+resultT['visitado'], resultT['tempoInicio'], resultT['antecessor'], resultT['tempoTermino'] = buscaProfundidadeAdaptado(Gt, sortTempo)
+print(resultT)
+print(Gt.arestas)
 
-print('Antecessor:')
-for vertice, antecessor in antecessores.items():
-    print(vertice, '->', antecessor)
-'''
+print(tempoInicio)
+print(tempoTermino)
+
+forteCon = []
+
+antList = list()
+
+for antecessor in resultT['antecessor']:
+    antList.append(antecessor)
+
+while antList:
+    ant = antList.pop(0)
+    conSet = buscaAntecessor(ant, resultT['antecessor'])
+    if conSet != set():
+        forteCon.append(conSet)
+    for x in conSet:
+        try:
+            antList.remove(x)
+        except:
+            pass
+
+print(forteCon)
--- A2/q2.py
+++ A2/q2.py
@@ -4,7 +4,6 @@ from bibGrafos import Grafo
 visitado = {}
 tempoInicio = {}
 tempoTermino = {}
-antecessor = {}
 time = 0
 list = []
 
@@ -12,26 +11,23 @@ def buscaProfundidade(grafo):
     global visitado
     global tempoInicio
     global tempoTermino
-    global antecessor
     global time
 
     for vertice in range(1,  grafo.tamanho+1):
         visitado[vertice] = False
         tempoInicio[vertice] = math.inf
         tempoTermino[vertice] = math.inf
-        antecessor[vertice] = None
 
     for vertice in range(1,  grafo.tamanho+1):
         if visitado[vertice] == False:
             visitaProfundidade(grafo, vertice)
  
-    return (visitado, tempoInicio, antecessor, tempoTermino)
+    return (visitado, tempoInicio, tempoTermino)
 
 def visitaProfundidade(grafo, vertice):
     global visitado
     global tempoInicio
     global tempoTermino
-    global antecessor
     global time
     global list
 
@@ -43,7 +39,6 @@ def visitaProfundidade(grafo, vertice):
 
     for vizinho in vizinhos:
         if visitado[vizinho] == False:
-            antecessor[vizinho] = vertice
             visitaProfundidade(grafo, vizinho)
     
     time = time+1
@@ -52,10 +47,12 @@ def visitaProfundidade(grafo, vertice):
 
     return True
 
-'''
-G = Grafo(arquivo='teste1.grafo')
+G = Grafo(arquivo='manha.net')
 
 buscaProfundidade(G)
+string = ''
 
-print(list)
-'''
+for vertice in list:
+    string = string+G.rotulo(vertice)+" -> "
+
+print(string[:-3])
