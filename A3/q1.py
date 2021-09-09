62d80ffeee34b819bfad638c24836369cce37ea5
Zyrotot <mathiasdutranichele@gmail.com>
2021-09-09 14:31:17 -0300

    Tirando retorno e criando residual

--- A3/q1.py
+++ A3/q1.py
@@ -1,12 +1,14 @@
 import math
 from bibGrafos import Grafo
+from copy import deepcopy
 
 def checaRetorno(grafo):
+    grafo_retorno = grafo
     for vertice in range(1, grafo.qtdVertices()+1):
         for vizinho in grafo.vizinhos(vertice):
             if vertice in grafo.vizinhos(vizinho):
-                removeRetorno(grafo, vertice, vizinho)
-    return None
+                grafo_retorno = removeRetorno(grafo, vertice, vizinho)
+    return grafo_retorno
 
 def removeRetorno(grafo, vertice, vizinho):
     peso = grafo.arestas[vertice, vizinho]
@@ -17,10 +19,34 @@ def removeRetorno(grafo, vertice, vizinho):
     tamanho = grafo.tamanho
     grafo.arestas[(vertice, tamanho)] = peso 
     grafo.arestas[(tamanho, vizinho)] = peso 
+    return grafo
+
+def geraResidual(grafo):
+    grafo_residual = grafo
+    arestas = grafo_residual.arestas
+    extra = {}
+    for aresta in arestas:
+        key = (aresta[1], aresta[0])
+        extra[key] = 0
+    grafo_residual.arestas = {**arestas, **extra}
+    return grafo_residual
+
+def fordFulkerson(grafo, fonte, sorvedouro, residual):
     return None
 
+def fluxoMaximo(grafo):
+    tamanho = grafo.tamanho
+    grafo_retorno = checaRetorno(grafo)
+    grafo_residual = deepcopy(grafo)
+    grafo_residual = geraResidual(grafo_residual)
+    
+    fordFulkerson(grafo_retorno, 1, tamanho, grafo_residual)
 
-G = Grafo(arquivo='instancias/fluxo_maximo/wiki.net')
-#G = Grafo(arquivo='instancias/fluxo_maximo/fluxo_maximo_aula.net')
+    print(grafo_retorno.arestas)
+    print(grafo_residual.arestas)
+    return None
 
-result = checaRetorno(G)
+#G = Grafo(arquivo='instancias/fluxo_maximo/wiki.net')
+G = Grafo(arquivo='instancias/fluxo_maximo/fluxo_maximo_aula.net')
+
+result = fluxoMaximo(G)
