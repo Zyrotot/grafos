53681f9fca96024f5d41a11c2f1587781ef07818
Zyrotot <mathiasdutranichele@gmail.com>
2021-09-21 09:39:49 -0300

    Avançando Q2

--- A3/q2.py
+++ A3/q2.py
@@ -2,6 +2,54 @@ import math
 from bibGrafos import Grafo
 from copy import deepcopy
 
+def separa(grafo):
+    x = deepcopy(grafo.vertices)
+
+    for vertice in x:
+        for vizinho in grafo.vizinhos(grafo.indice(vertice)):
+            try:
+                x.remove(grafo.rotulo(vizinho))
+            except:
+                pass
+
+    return x
+
+
+def BFS(grafo, mate, xc, distancia):
+    queue = []
+
+    for x in xc:
+        if mate[grafo.indice(x)] == None:
+            distancia[grafo.indice(x)] = 0
+            queue.append(grafo.indice(x))
+        else:
+            distancia[x] = math.inf
+    print(distancia)
+    t = grafo.tamanho+1
+    distancia[t] = math.inf
+
+    print(distancia)
+
+    while queue:
+        x = queue.pop(0)
+        if distancia[x]<distancia[t]:
+            for vizinho in grafo.vizinhos(x):
+                if distancia[mate[vizinho]] == math.inf: #WTF
+                    distancia[mate[vizinho]] = distancia[x] + 1
+                    queue.append(mate[vizinho])
+
+    state = distancia[t] != math.inf
+
+    print(state)
+
+    return (state)
+
+def DFS(grafo, mate, x, distancia):
+    state = False
+
+
+    return state
+
 def hopcroftKarp(grafo):
     distancia = {}
     mate = {}
@@ -11,35 +59,17 @@ def hopcroftKarp(grafo):
     for vertice in range(1, grafo.tamanho+1):
         distancia[vertice] = math.inf
         mate[vertice] = None
-    
+
+    xc = separa(grafo)
+
+    while BFS(grafo, mate, xc, distancia):
+        for xi in xc:
+            if mate[xi]==None:
+                if DFS(grafo, mate, xi, distancia):
+                    m = m+1
+        
     return m, mate
-
-def separa(grafo):
-    x = deepcopy(grafo.vertices)
-
-    for vertice in x:
-        for vizinho in grafo.vizinhos(int(vertice)):
-            try:
-                x.remove(str(vizinho))
-            except:
-                pass
-
-    return x
-
-
-def BFS(grafo, mate, distancia):
-    state = False
-
-    return state
-
-def DFS(grafo, mate, distancia):
-    state = False
-
-
-    return state
 
 G = Grafo(arquivo='instancias/emparelhamento_maximo/pequeno.net')
 
-#num, result = hopcroftKarp(G)
-x = separa(G)
-print(x)
+num, result = hopcroftKarp(G)
