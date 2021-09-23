22ab3f94b2cb488f41f42a73d8c82fd285aacddd
Zyrotot <mathiasdutranichele@gmail.com>
2021-09-23 17:21:01 -0300

    Terminando 3

--- A3/q3.py
+++ A3/q3.py
@@ -24,50 +24,49 @@ def montas(grafo):
 
     return s
 
-def achaMaximal(grafo, s, x):
-    for num in s:
-        if num != 0:
-            x[num] = math.inf
-            tempG = deepcopy(grafo)
-            tempG.vertices = []
-            tempG.arestas = {}
-            for vertice in s[num]:
-                tempG.vertices.append(vertice)
-                for v2 in s[num]:
-                    if (grafo.indice(vertice), grafo.indice(v2)) in grafo.arestas.keys():
-                        tempG.arestas[grafo.indice(vertice), grafo.indice(v2)] = grafo.peso(grafo.indice(vertice), grafo.indice(v2))
-                    elif (grafo.indice(v2), grafo.indice(vertice)) in grafo.arestas.keys():
-                        tempG.arestas[grafo.indice(v2), grafo.indice(vertice)] = grafo.peso(grafo.indice(v2), grafo.indice(vertice))
+def achaMaximal(grafo, s, tempG):
+    maximal = {0:[]}
+    para = False
+    for vertice in tempG.vertices:
+        contador = 0
+        while True:
+            if para:
+                para = False
+                break
+            try:
+                if isinstance(maximal[contador], list):
+                    if not maximal[contador]:
+                        maximal[contador].append(vertice)
+                        para = True
+                    else:
+                        tem = False
+                        for v2 in maximal[contador]:
+                            if para:
+                                break
+                            if (grafo.indice(vertice), grafo.indice(v2)) in tempG.arestas.keys() or (grafo.indice(v2), grafo.indice(vertice)) in tempG.arestas.keys():
+                                tem = True
+                                contador = contador + 1
+                        if not tem:
+                            maximal[contador].append(vertice)
+                            para = True
+            except:
+                maximal[contador] = []
 
-            maximal = {0:[]}
-            para = False
-            for vertice in tempG.vertices:
-                contador = 0
-                while True:
-                    if para:
-                        para = False
-                        break
-                    try:
-                        if isinstance(maximal[contador], list):
-                            if not maximal[contador]:
-                                maximal[contador].append(vertice)
-                                para = True
-                            else:
-                                tem = False
-                                for v2 in maximal[contador]:
-                                    if para:
-                                        break
-                                    if (grafo.indice(vertice), grafo.indice(v2)) in tempG.arestas.keys() or (grafo.indice(v2), grafo.indice(vertice)) in tempG.arestas.keys():
-                                        tem = True
-                                        contador = contador + 1
-                                if not tem:
-                                    maximal[contador].append(vertice)
-                                    para = True
-                    except:
-                        maximal[contador] = []
-        else:
-            x[num] = 0
-    return None
+    return maximal
+
+def geraTempG(s, grafo):
+    tempG = deepcopy(grafo)
+    tempG.vertices = []
+    tempG.arestas = {}
+    for vertice in s:
+        tempG.vertices.append(vertice)
+        for v2 in s:
+            if (grafo.indice(vertice), grafo.indice(v2)) in grafo.arestas.keys():
+                tempG.arestas[grafo.indice(vertice), grafo.indice(v2)] = grafo.peso(grafo.indice(vertice), grafo.indice(v2))
+            elif (grafo.indice(v2), grafo.indice(vertice)) in grafo.arestas.keys():
+                tempG.arestas[grafo.indice(v2), grafo.indice(vertice)] = grafo.peso(grafo.indice(v2), grafo.indice(vertice))
+
+    return tempG
 
 def lawler(grafo):
     max = math.pow(2, grafo.tamanho)
@@ -78,11 +77,27 @@ def lawler(grafo):
 
     s = montas(grafo)
 
-    achaMaximal(grafo, s, x)
+    for num in s:
+        if num != 0:
+            x[num] = math.inf
+            tempG = geraTempG(s[num], grafo)
+            maximal = achaMaximal(grafo, s, tempG)
+            for grupo in maximal:
+                i = list(set(tempG.vertices).difference(maximal[grupo]))
+                for con in s:
+                    tempS = s[con]
+                    tempS.sort()
+                    tempI = i
+                    tempI.sort()
+                    if tempS == tempI:
+                        if x[con]+1 < x[num]:
+                            x[num] = x[con]+1
+        else:
+            x[num] = 0
 
-    return x
+    return x[num], maximal
 
 G = Grafo(arquivo='instancias/coloracao/cor3.net')
 
-x = lawler(G)
-s = montas(G)
+x, maximal = lawler(G)
+print('Número de cores usadas:', x, '/ Cores por vértice:', maximal)
